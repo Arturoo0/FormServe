0d173a887cd57ef32932102529c9ce0c4040be6a
Arturoo0 <arturoportelles@gmail.com>
2021-11-22 16:34:13 -0500

    add env depend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config(); 
+const express = require('express'); 
+const app = express();
+const port = 3000; 
 
 console.log(process.env.TEST);
 
