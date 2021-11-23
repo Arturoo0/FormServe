7942e44717f6873606ec4d666b5c9ae07b194cea
Arturoo0 <arturoportelles@gmail.com>
2021-11-23 18:51:44 -0500

    Create init user schema

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: True
+        },
+        username: {
+            type: String,
+            required: True
+        },
+        password: {
+            type: String,
+            required: True
+        }, 
+        id: {
+            type: String,
+            default: uuidv4
+        }
+    }
+);
