47b4ffcd15a62f909a1d723ce2fb4d43f38b0919
Arturoo0 <arturoportelles@gmail.com>
2021-11-22 21:46:51 -0500

    factory method

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,34 @@
 require('dotenv').config(); 
-const express = require('express'); 
-const app = express();
-const port = 3000; 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const cors = require('cors');
+const express = require('express'); 
+const cookieParser = require('cookie-parser');
+
+const appStartTime = Date.now();
+
+const startApp = async () => {
+  const port = 3000;
+  const app = express();
+
+  app.use(cors({
+    credentials: true 
+  }));
+  app.use(cookieParser());
+  app.use(express.urlencoded({
+      extended: true
+  }));
+  app.use(express.json()); 
+
+  app.get('/', (req, res) => {
+    const status = {
+      uptime: Date.now() - appStartTime
+    }
+    res.send(status);
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+  })
+}
+
+startApp();
