404dcc1dfe402146ea0b617106c6c28bfe564cb5
Arturoo0 <arturoportelles@gmail.com>
2022-01-03 13:51:25 -0500

    Remove backend log on request

--- backend/routes/forms.js
+++ backend/routes/forms.js
@@ -6,7 +6,6 @@ const formRouter = express.Router();
 formRouter.use(authenticate());
 
 formRouter.post('/create', async (req, res) => {
-    console.log(req.userEmail);
     const doc = await User.findOneAndUpdate(
         { email: req.userEmail },
         { $push: { documents: {}}}
