d9f399805584888df81a702e9ce45bef82995d93
Arturoo0 <arturoportelles@gmail.com>
2022-01-03 14:41:30 -0500

    Return error message on max document capacity

--- backend/routes/forms.js
+++ backend/routes/forms.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const authenticate = require('../middleware/authenticateSession.js');
 const { User } = require('../models/User.js');
+const errorMessages = require('../utils/errorMessages.js');
 
 const formRouter = express.Router();
 formRouter.use(authenticate());
 
 formRouter.post('/create', async (req, res) => {
-    const doc = await User.findOneAndUpdate(
-        { email: req.userEmail },
-        { $push: { documents: {}}}
-    );
-    return res.send({}) 
+    const doc = await User.findOne({ email: req.userEmail });
+    const { documents, maxDocuments } = doc;
+    if (documents.length > maxDocuments) {
+        return res.send(errorMessages.maxDocumentLimitExceeded);
+    } else{
+        const doc = await User.findOneAndUpdate(
+            { email: req.userEmail },
+            { $push: { documents: {}}}
+        );
+    };
+    return res.send({});  
 });
 
 module.exports = formRouter;
